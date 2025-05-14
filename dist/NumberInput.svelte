e405df27dce5b7adb9ce12c947bcbffd02a969ea
rexbrandy <bailey.sdev@gmail.com>
2025-05-14 13:04:35 +1000

    Added Percentage input

--- dist/NumberInput.svelte
+++ dist/NumberInput.svelte
@@ -1,57 +1,50 @@
 <script lang="ts">
-    import BaseInput from './BaseInput.svelte';
-    import type { FormState, FieldState } from './Interfaces';
-  
-  interface Props {
-    label?: string;
-    value?: number | null;
-    required?: boolean;
-    name?: string;
-    disabled?: boolean;
-    formState?: FormState | null;
-    classes?: string;
-    on_change?: any;
-    placeholder?: string;
-    min?: number | null;
-    max?: number | null;
-  }
+	import BaseInput from './BaseInput.svelte';
+	import type { FormState, FieldState } from './Interfaces';
 
-  let {
-    label = '',
-    value = $bindable(null),
-    required = false,
-    name = '',
-    disabled = false,
-    formState = null,
-    classes = 'smart-form-input',
-    on_change = () => {},
-    placeholder = '',
-    min = null,
-    max = null
-  }: Props = $props();
-    let fieldState: FieldState = $state();
-  </script>
-  
-  <BaseInput
-    label={label}
-    required={required}
-    classes={classes}
-    name={name}
-    bind:fieldState={fieldState}
-    bind:value={value}
-    formState={formState}
-  >
-    <input
-    onblur={() => {fieldState.blur()}}
-    onkeyup={on_change}
-    required={required}
-    disabled="{disabled}"
-    placeholder="{placeholder}"
-    type="number"
-    name="{name}"
-    bind:value="{value}"
-    min="{min}"
-    max="{max}"
-    />
-  </BaseInput>
-  
+	interface Props {
+		label?: string;
+		value?: number | null;
+		required?: boolean;
+		name?: string;
+		disabled?: boolean;
+		formState?: FormState | null;
+		classes?: string;
+		on_change?: any;
+		placeholder?: string;
+		min?: number | null;
+		max?: number | null;
+	}
+
+	let {
+		label = '',
+		value = $bindable(null),
+		required = false,
+		name = '',
+		disabled = false,
+		formState = null,
+		classes = 'smart-form-input',
+		on_change = () => {},
+		placeholder = '',
+		min = null,
+		max = null
+	}: Props = $props();
+	let fieldState: FieldState = $state();
+</script>
+
+<BaseInput {label} {required} {classes} {name} bind:fieldState bind:value {formState}>
+	<input
+		onblur={() => {
+			fieldState.blur();
+		}}
+		onkeyup={on_change}
+		{required}
+		{disabled}
+		{placeholder}
+		type="number"
+		{name}
+		bind:value
+		{min}
+		{max}
+	/>
+</BaseInput>
